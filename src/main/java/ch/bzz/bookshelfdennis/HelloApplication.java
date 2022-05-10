efa288c6b409ef1ddfe9be66ebcc780e0876e815
Easy_MR60 <woodtlid@bzz.ch>
2022-05-10 08:57:49 +0200

    works
    works

---
+++ src/main/java/ch/bzz/bookshelfdennis/HelloApplication.java
@@ -0,0 +1,9 @@
+package ch.bzz.bookshelfdennis;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
